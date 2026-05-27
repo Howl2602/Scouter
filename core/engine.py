f8aa86733abb366d62c3dcebdc07a8c9a6815b06
Vo Minh Dien <vominhdien666@gmail.com>
2026-05-27 12:28:58 +0700

    Fix: Rename config file to settings.yaml and clean repository

---
+++ core/engine.py
@@ -0,0 +1,7 @@
+from core.notifier import send_message
+
+
+def run():
+    print("[INFO] Engine started")
+
+    send_message("Scouter started 😭")
--- core/notifier.py
+++ core/notifier.py
@@ -1,39 +1,26 @@
 import yaml
+from pathlib import Path
 import sys
 import requests
 
 
-def load_config(path="./setting.yaml"):
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+CONFIG_FILE = BASE_DIR / "config" / "settings.yaml"
+
+
+def load_config():
     try:
-        with open(path, "r", encoding="utf-8") as f:
+        with open(CONFIG_FILE, "r", encoding="utf-8") as f:
             config = yaml.safe_load(f)
 
-        # File trống
         if not config:
-            print("[ERROR] setting.yaml is empty!")
-            sys.exit(1)
-
-        # Check telegram section
-        if "telegram" not in config:
-            print("[ERROR] Missing [telegram] section!")
-            sys.exit(1)
-
-        telegram = config["telegram"]
-
-        # Check token
-        if not telegram.get("token"):
-            print("[ERROR] Telegram bot token is empty!")
-            sys.exit(1)
-
-        # Check chat_id
-        if not telegram.get("chat_id"):
-            print("[ERROR] Telegram chat_id is empty!")
-            sys.exit(1)
+            raise ValueError("settings.yaml is empty!")
 
         return config
 
     except FileNotFoundError:
-        print("[ERROR] setting.yaml not found!")
+        print("[ERROR] settings.yaml not found!")
         sys.exit(1)
 
     except yaml.YAMLError as e:
@@ -45,10 +32,10 @@ def load_config(path="./setting.yaml"):
         sys.exit(1)
 
 
-config = load_config()
+CONFIG = load_config()
 
-BOT_TOKEN = config["telegram"]["token"]
-CHAT_ID = config["telegram"]["chat_id"]
+BOT_TOKEN = CONFIG["telegram"]["token"]
+CHAT_ID = CONFIG["telegram"]["chat_id"]
 
 
 def send_message(message):
